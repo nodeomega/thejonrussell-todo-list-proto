109962457e67c7dd8149d3b5d11f9d5bb8f50d0a
Jon Russell <jrussell@nodeomega.com>
2023-09-07 21:14:47 -0700

    Ensure usage of tmp directory due to read-only on serverless Vercel app.

--- src/app/api/staticdata/route.ts
+++ src/app/api/staticdata/route.ts
@@ -6,11 +6,13 @@ import ShopifyToDoItemList from '@/app/models/shopify-todo-item-list';
 
 //Set the absolute path of the json directory
 const jsonDirectory = path.join(process.cwd(), 'json');
+const tmpDirectory = path.join(process.cwd(), 'tmp');
 const fileName = "/demo-todo.json";
 
 export async function GET(req: NextRequest) { //, res: NextApiResponse) {
   //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + fileName, 'utf8');
+  await checkToDoTempExists();
+  const fileContents = await fs.readFile(tmpDirectory + fileName, 'utf8');
   //Return the content of the data file in json format
   return NextResponse.json(JSON.parse(fileContents));
 }
@@ -22,7 +24,7 @@ export async function PUT(req: NextRequest) {
   // Save the file.
   saveToDoState(todoItems);
   //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + fileName, 'utf8');
+  const fileContents = await fs.readFile(tmpDirectory + fileName, 'utf8');
   //Return the content of the data file in json format
   return NextResponse.json(JSON.parse(fileContents));  
 }
@@ -35,11 +37,20 @@ export async function DELETE(req: NextRequest) {
   // Save the file.
   saveToDoState(todoItems);
   //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory  + fileName, 'utf8');
+  const fileContents = await fs.readFile(tmpDirectory  + fileName, 'utf8');
   //Return the content of the data file in json format
   return NextResponse.json(JSON.parse(fileContents));  
 }
 
+async function checkToDoTempExists() {
+  try {
+    await fs.readFile(tmpDirectory  + fileName, 'utf8');
+  } catch {
+    const fileContents = await fs.readFile(jsonDirectory  + fileName, 'utf8');
+    writeFileSync(tmpDirectory + fileName, fileContents, 'utf8');
+  }
+}
+
 function saveToDoState(data: ShopifyToDoItemList) {
-  writeFileSync(jsonDirectory + fileName, JSON.stringify(data, null, 2), 'utf8');
+  writeFileSync(tmpDirectory + fileName, JSON.stringify(data, null, 2), 'utf8');
 }
